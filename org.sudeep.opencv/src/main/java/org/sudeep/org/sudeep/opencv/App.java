358d9b338dc51d387de246e744265c43fd06ed7d
Ubuntu <brajeshp@ubuntuvm.iryeoibi0ayuzez5f00rd5uada.rx.internal.cloudapp.net>
2020-04-03 04:25:09 +0000

    ubuntu so file

--- org.sudeep.opencv/src/main/java/org/sudeep/org/sudeep/opencv/App.java
+++ org.sudeep.opencv/src/main/java/org/sudeep/org/sudeep/opencv/App.java
@@ -12,10 +12,11 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
         System.out.println( "Hello World!" );
         System.out.println("Welcome to OpenCV " + Core.VERSION);
         System.out.println(System.getProperty("java.library.path"));
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
          Mat m  = Mat.eye(3, 3, CvType.CV_8UC1);
         System.out.println("m = " + m.dump());
         
